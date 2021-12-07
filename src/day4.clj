6caa0c56927ad055dde8703eb257a899bb727f09
Josh Reighley <josh@reighley.net>
2021-12-06 22:51:26 -0800

    finished day 4

--- src/day4.clj
+++ src/day4.clj
@@ -4,7 +4,7 @@
                      (clojure.string/split-lines)
                      (remove #(= "" %))))
 
-(def numbers-call-list (map #(Long/parseLong %)(clojure.string/split (first data-lines) #",")))
+(def n-call-seq (mapv #(Long/parseLong %)(clojure.string/split (first data-lines) #",")))
 
 (defn cols [card]
   (for [col (range 5)]
@@ -24,88 +24,37 @@
 
 (def bingo-cards
   (->> (rest data-lines)
-      (map process-line)
-      (partition 5)))
+       (map process-line)
+       (partition 5)))
 
-(def card-win-sets  (map win-sets bingo-cards))
+(defn call-order [n]
+   (.indexOf n-call-seq n))
 
-(defn winner? [nlist card-pos]  (clojure.set/superset? (set nlist) card-pos))
+(defn find-win [win-sets]
+  (reduce min (for [ws win-sets]
+                (->> ws
+                 (map call-order)
+                 (reduce max)))))
 
-(defn anywinner? [nlist] (remove empty? (for [card card-win-sets]
-                                            (if ((complement empty?) (filter #(winner? nlist %) card))
-                                              card))))
+(defn card-score [called-ns card]
+   (->> card
+        (flatten)
+        (remove (set called-ns))
+        (reduce +)
+        (* (last called-ns))))
 
-(defn calc-sum-unmaprked [wc]
-  (->> wc
-       (first)
-       (reduce into #{})
-       (remove (set (take 22 numbers-call-list)))
-       (remove #(= % ""))
-       (map #(Long/parseLong %))
-       (reduce +)))
+(defn find-win-info [card]
+  (let [win-combos (win-sets card)
+        earliest-win (find-win win-combos)
+        called-numbers (take (inc earliest-win) n-call-seq)]
+    {:win-turn earliest-win
+     :card card
+     :called-numbers called-numbers
+     :score (card-score called-numbers card)}))
 
-(def winning-card (->> (take 22 numbers-call-list)
-                       (anywinner?)))
+(def scored-cards (sort-by :win-turn (map #(find-win-info % ) bingo-cards)))
 
-(def call-sets (for [n (range 1 (count  numbers-call-list))]
-                 (set  (take n numbers-call-list))))
-
-(defn card-info [card]
-  (let [card-win-cond (win-sets card)]
-      card-win-cond))
-
-(defn shortest-superset [set]
-  (let [called-digits (->> call-sets
-                           (filter #(clojure.set/superset? % set))
-                           (sort-by #(count %))
-                           (first))
-        n-digits (count called-digits)]
-    {:called called-digits :number  n-digits}))
-
-(defn find-winnning-call [card]
-  (let [winning-sets (->> card
-                          (win-sets)
-                          (map shortest-superset)
-                          (sort-by :number)
-                          (first))]
-    (assoc winning-sets
-      :card card)))
-
-(def losing-card (->> (map find-winnning-call bingo-cards)
-                      (filter #(pos? (:number %)))
-                      (sort-by :number)
-                      (last)))
-
-(def winning-card (->> (map find-winnning-call bingo-cards)
-                       (filter #(pos? (:number %)))
-                       (sort-by :number)
-                       (first)))
-
-(* (->> winning-card
-        (:card)
-        (reduce into)
-        (set)
-        (remove :called)
-        (reduce +))
-
-  (->> winning-card
-       :number
-       dec
-       (nth numbers-call-list)))  ;should be 67716
-
-;65 wrong -- probalby off by one?
-;89895 too high  12447 also too high..
-
-(keep (set (->> losing-card
-                :called)) numbers-call-list) ;9
+(def answer-1 (:score (first scored-cards))) ;67716
+(def answer-2 (:score (last scored-cards)))  ;1830
 
 
-
-
-(defn checkwin? [win-conds called-list]
-   (filter #(clojure.set/superset? called-list %) win-conds))
-(def bingo? #((complement empty?) %))
-
-
-(comment "66 is the number"
-  win-sets)
