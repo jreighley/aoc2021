3594c66424ba50afc75e41571e9ad1d247ca7b3c
Josh Reighley <Josh@reighley.net>
2021-12-08 09:45:00 -0800

    day8 completed

--- src/day8.clj
+++ src/day8.clj
@@ -1,4 +1,5 @@
-(ns day8)
+(ns day8
+ (:require [ clojure.set :as cs]))
 
 (def data (->> (slurp "resources/day8")
                (clojure.string/split-lines)))
@@ -23,16 +24,50 @@
     (for [line data]
       {:signal (group-by #(.length %) (take 10 line)) :output (take-last 4 line)})))
 
+(defn set->str [k]
+  (->> k
+       sort
+       (apply str)))
 
+(defn calc-line-decryter [{:keys [signal output]}]
+      (let  [one (set (first  (get signal 2)))
+             four (set (first  (get signal 4)))
+             seven (set (first  (get signal 3)))
+             eight (set (first  (get signal 7)))
+             five-segs (map set  (get signal 5))
+             six-segs (map set  (get signal 6))
+             nine (first  (filter #(cs/subset?  four %) six-segs))
+             top-seg (cs/difference seven one)
+             three (first  (filter #(cs/subset?  seven %) five-segs))
+             upper-left-seg (cs/difference four three)
+             five (first (filter #(cs/subset? upper-left-seg  %) five-segs))
+             two (first  (remove #{three five} five-segs))
+             upper-right (cs/difference nine five)
+             six (set (remove upper-right eight))
+             zero (first (remove #{six nine } six-segs))
 
-(defn compute-digit [facts digit]
-  (let [novel-fact (cond (= 2 (.length digit)) {(set digit) 1}
-                         (= 3 (.length digit)) {(set digit) 7}
-                         (= 4 (.length digit)) {(set digit) 4}
-                         (= 8 (.length digit)) {(set digit) 8})]
-       (conj facts novel-fact)))
+             decrypter {(set->str one) 1
+                        (set->str two) 2
+                        (set->str three) 3
+                        (set->str four) 4
+                        (set->str five) 5
+                        (set->str six) 6
+                        (set->str seven) 7
+                        (set->str eight) 8
+                        (set->str nine) 9
+                        (set->str zero) 0}
+             sorted-output (map #(-> %
+                                     set
+                                     set->str)output)
+             output-digits (map decrypter sorted-output)]
+        (Long/parseLong (apply str output-digits))))
 
+(defn calc-answer-2 [input]
+  (reduce + (map calc-line-decryter input)))
 
+(calc-answer-2 datap2) ;1016804 correct
 
-(comment (think {} '("af" "bafc" "fea" "af")))
+(comment  (->> (get  (:signal  (first datap2))6)))
+
+(comment (calc-line-decryter (first datap2)))
 
